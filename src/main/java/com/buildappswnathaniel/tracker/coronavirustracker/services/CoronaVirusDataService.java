2f083076a7383ee24f9110b44653d1ea322e358f
Nathaniel Mphisa <nathymphisa@gmail.com>
2021-07-30 18:29:52 +0200

    First Commit - to solve Thymeleaf renderring error

---
+++ src/main/java/com/buildappswnathaniel/tracker/coronavirustracker/services/CoronaVirusDataService.java
@@ -0,0 +1,57 @@
+package com.buildappswnathaniel.tracker.coronavirustracker.services;
+
+import com.buildappswnathaniel.tracker.coronavirustracker.model.LocationStats;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CoronaVirusDataService {
+    // this is a service class that will fetch data for us.
+    // when the application calls its going to make a call for the data using a url
+
+//    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/07-28-2020.csv";
+    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/archived_data/archived_time_series/time_series_2019-ncov-Confirmed.csv";
+    private List<LocationStats> locationStats = new ArrayList<>();
+
+    public List<LocationStats> getLocationStats() {
+        return locationStats;
+    }
+
+    @PostConstruct
+    @Scheduled(cron = "* * 1 * * *")
+    public void fetchVirusData() throws IOException, InterruptedException {
+        List<LocationStats> list = new ArrayList<>();
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(VIRUS_DATA_URL))
+                .build();
+        HttpResponse<String> httpResponse = client.send(request,HttpResponse.BodyHandlers.ofString());
+
+        StringReader cvsBodyReader = new StringReader(httpResponse.body());
+
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(cvsBodyReader);
+        for (CSVRecord record : records) {
+
+              LocationStats stats = new LocationStats();
+              stats.setCountry(record.get("Country/Region"));
+              stats.setState(record.get("Province/State"));
+              stats.setTotalCases(Integer.parseInt(record.get(record.size() - 1)));
+              System.out.println(stats);
+              list.add(stats);
+        }
+        this.locationStats = list;
+    }
+}
+
